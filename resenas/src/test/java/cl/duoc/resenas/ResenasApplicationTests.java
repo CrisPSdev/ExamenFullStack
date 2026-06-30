8ecb317c5b2a2a03e69620473f6800405bd6b885
CrisPSdev <crispardos@pm.me>
2026-06-30 11:28:31 -0400

    edited - sin corroborar

--- resenas/src/test/java/cl/duoc/resenas/ResenasApplicationTests.java
+++ resenas/src/test/java/cl/duoc/resenas/ResenasApplicationTests.java
@@ -1,13 +1,14 @@
 package cl.duoc.resenas;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ResenasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void mainClass_isLoadable() {
+        ResenasApplication app = new ResenasApplication();
+        assertThat(app).isNotNull();
+    }
 }
